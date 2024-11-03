bf1ce6c62db0c630e5fbd1a568b308277d812243
Minh-Nguyen1307 <minh.nguyen1307s2@gmail.com>
2024-11-03 14:35:34 +0700

    .

--- index.js
+++ index.js
@@ -16,13 +16,28 @@ app.get('/customers/:id',(req,res)=>{
 })
 app.get('/customers/:customerId/orders' ,(req,res) =>{
     const {customerId} = req.params;
-    const order = orders.filter(o => o.customerId === customerId);
-    if(order) {
-        res.send(order);
+    const customerOrder = orders.filter(o => o.customerId === customerId);
+    if(customerOrder) {
+        res.send(customerOrder);
     } else {
         res.status(404).send({error:'Orders not found'})
     }
 
+})
+app.get('/orders/highvalue', (req,res) => {
+    const ordersHighvalue = orders.filter(p => p.totalPrice >10000000)
+    if(ordersHighvalue.length >0) {
+        res.send(ordersHighvalue)
+    } else {
+        res.status(404).send({error:'Orders not found'})
+    }
+})
+
+app.get('/products', (req, res) => {
+    
+})
+app.get('*', (req,res) => {
+    res.send('Not support!')
 })
 app.listen(8080, () => {
     console.log('Server is running!');
